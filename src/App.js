de4fe98b8610b1b56f3d6729e94c35967d1b1a7f
Ajay B L <be.el.ajay@gmail.com>
2021-01-29 00:05:41 +0530

    Adding portfolio-app to react route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SigninPage from './pages/signin';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio-app">
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
